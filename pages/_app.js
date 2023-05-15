14580fec5bc350c98cf9d1f5f2ffcdb9d24e29ee
Kevin Ketner <kette92@gmail.com>
2023-05-15 10:41:31 +0200

    finish feature mongoDb

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,10 @@
-import Layout from "@/components/Layout/Layout";
 import GlobalStyle from "../styles";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Component {...pageProps} />
     </>
   );
 }
