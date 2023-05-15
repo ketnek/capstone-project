f37cc58265a430e13a2500399fb575c1b1304e14
Kevin Ketner <kette92@gmail.com>
2023-05-15 11:01:58 +0200

    update style

--- components/Control/StyledControl.js
+++ components/Control/StyledControl.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export default styled.div`
   position: absolute;
-  top: 4rem;
-  right: 1rem;
+  top: 2rem;
+  right: 2rem;
   z-index: 1;
   display: flex;
   flex-direction: column;
--- styles.js
+++ styles.js
@@ -2,17 +2,6 @@ import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
 
-/* color variables */
-
-:root {
-  --orange: #FF4A11;
-  --dark-gray: #252629;
-  --white: white;
-  --light-gray: #F2F5F9;
-  --icon-height: 2.5rem;
-  --icon-width: 2.5rem; 
-}
-
   *,
   *::before,
   *::after {
