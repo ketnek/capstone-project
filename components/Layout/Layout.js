d1a96193bc3711e42694aa2cf4f3cd6965bf2646
Kevin Ketner <kette92@gmail.com>
2023-05-15 10:55:17 +0200

    add layout

---
+++ components/Layout/Layout.js
@@ -0,0 +1,41 @@
+import {
+  BikeIcon,
+  Footer,
+  Header,
+  Headline,
+  MapIcon,
+  Nav,
+  NavLink,
+  RouteIcon,
+  StarIcon,
+} from "./StyledLayout";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Header>
+        <Headline>Map</Headline>
+      </Header>
+      <main>{children}</main>
+      <Footer>
+        <Nav>
+          <NavLink href="#">
+            <MapIcon />
+          </NavLink>
+
+          <NavLink href="#">
+            <RouteIcon />
+          </NavLink>
+
+          <NavLink href="#">
+            <StarIcon />
+          </NavLink>
+
+          <NavLink href="#">
+            <BikeIcon />
+          </NavLink>
+        </Nav>
+      </Footer>
+    </>
+  );
+}
