4fa1a64c9a0912d47f574cc2e331749d00dae055
Kevin Ketner <kette92@gmail.com>
2023-05-15 10:29:37 +0200

    update Control component

--- components/ControlButton/ControlButton.js
+++ components/ControlButton/ControlButton.js
@@ -1,9 +1,9 @@
 import Button from "./StyledControlButton";
 import { FaRegSave, FaTrashAlt, FaCheck, FaRedoAlt } from "react-icons/fa";
 
-export default function ControlButton({ job, handler, isLoading, calculated }) {
+export default function ControlButton({ job, onClick, isLoading, calculated }) {
   return (
-    <Button disabled={isLoading} onClick={handler}>
+    <Button disabled={isLoading} onClick={onClick}>
       {job === "save" && <FaRegSave />}
       {job === "create" && (calculated ? <FaRedoAlt /> : <FaCheck />)}
       {job === "delete" && <FaTrashAlt />}
