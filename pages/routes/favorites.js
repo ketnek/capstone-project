7664db33b480db4b61a7d28744f62a5c1e641c1b
Kevin Ketner <kette92@gmail.com>
2023-05-18 07:53:55 +0200

    render fav cards on favorites page

--- pages/routes/favorites.js
+++ pages/routes/favorites.js
@@ -1,5 +1,14 @@
+import Card from "@/components/Card/Card";
 import Placeholder from "@/components/Placeholder/Placeholder";
 
-export default function Routes() {
-  return <Placeholder text="Which is your favorite route?" />;
+export default function Routes({ routes, error, isLoading, refetch }) {
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  const favoriteRoutes = routes.filter((route) => route.favorite);
+
+  if (favoriteRoutes.length === 0)
+    return <Placeholder text="Which is your favorite route?" />;
+
+  return <Card routes={favoriteRoutes} refetch={refetch} />;
 }
--- pages/routes/index.js
+++ pages/routes/index.js
@@ -1,19 +1,10 @@
-import useSWR from "swr";
 import Card from "@/components/Card/Card";
 import Placeholder from "@/components/Placeholder/Placeholder";
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
-
-export default function Routes() {
-  const {
-    data: routes,
-    error,
-    isLoading,
-    mutate: refetch,
-  } = useSWR("/api/routes", fetcher);
-
+export default function Routes({ routes, error, isLoading, refetch }) {
   if (error) return <div>failed to load</div>;
   if (isLoading) return <div>loading...</div>;
+
   if (routes.length === 0)
     return <Placeholder text="Add your first route today!" />;
 
