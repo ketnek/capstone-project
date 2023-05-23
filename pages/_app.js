a2965be227bf9fb6d52273ed782401adc26319b1
Kevin Ketner <kette92@gmail.com>
2023-05-23 07:59:02 +0200

    update

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import Layout from "@/components/Layout/Layout";
 
 // Change Token before git push!!!
 const accessToken =
-  "pk.eyJ1Ijoia2V0bmVrIiwiYSI6ImNsaHp2MGpkdDFobHIzbG13NTNsZWEwbGUifQ.d25xaSB_w9TQujEs1CD6mw";
+  "pk.eyJ1Ijoia2V0bmVrIiwiYSI6ImNsaHp2NXNyMTA5cXAzbnVuYm11MWFkcjgifQ.Heb3QWzmvHldeT0CFMITNA";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
