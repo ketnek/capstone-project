da5b1e73773fba783f534356dec54fd728641a3e
Kevin Ketner <kette92@gmail.com>
2023-05-23 07:50:43 +0200

    delete token

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,7 @@ import GlobalStyle from "../styles";
 import Layout from "@/components/Layout/Layout";
 
 // Change Token before git push!!!
-const accessToken =
-  "pk.eyJ1Ijoia2V0bmVrIiwiYSI6ImNsaHVraTRkYjAwcWIzY2xzM2x2NGJtdmkifQ.kbSc3NZC1GJcOK0rJ2RFiQ";
+const accessToken = "";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
