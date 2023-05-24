f3b66aba5cf5555c1283e41fe3892f7c4ba4974a
Kevin Ketner <kette92@gmail.com>
2023-05-24 15:23:59 +0200

    update

--- components/TextInput/StyeldTextInput.js
+++ components/TextInput/StyeldTextInput.js
@@ -4,5 +4,5 @@ const Label = styled.label`
   font-size: 1rem;
   font-weight: bold;
 `;
-
+// I will add more styling here :-)
 export { Label };
--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import Layout from "@/components/Layout/Layout";
 
 // Change Token before git push!!!
 const accessToken =
-  "pk.eyJ1Ijoia2V0bmVrIiwiYSI6ImNsaHp2NXNyMTA5cXAzbnVuYm11MWFkcjgifQ.Heb3QWzmvHldeT0CFMITNA";
+  "pk.eyJ1Ijoia2V0bmVrIiwiYSI6ImNsaHp2NXNyMTA5cXAzbnVuYm11MWFkc";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
